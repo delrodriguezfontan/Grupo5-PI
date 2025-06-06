f014915104a2fd6a8334c38588e71ba09e3b568a
delrodriguezfontan <drodriguezfontan@udesa.edu.ar>
2025-06-06 09:48:47 -0300

    termine seriesgenres

--- js/seriesgenres.js
+++ js/seriesgenres.js
@@ -1,19 +1,32 @@
 
+let selector = document.querySelector(".listado")
+
 let api = "https://api.themoviedb.org/3/genre/tv/list?api_key=7af9e68f00d96b306cc0ab2e52ceaf9c";
 
-fetch(apiKey)
+fetch(api)
 .then(function(response) {
     return response.json()
   })
-  .then(function(data){let container = document.querySelector(".listado");
+  .then(function(data){
     let genres = data.genres; 
 
+    console.log(genres)
+    
     for (let i = 0; i < genres.length; i++) {
-        let genre = genres[i];
-        container.innerHTML +=
+        const element = genres[i];
+        console.log(element);
         
+        selector.innerHTML +=
+        `<ul class="listado"> <a href="./detail-genres.html?id=${element.id}"
+        <li class="s"> ${element.name} </li></a> `
 
-    }})
-    .catch(function(error) {
-        console.log("Error: " + error);
-      })
+    }
+})
+.catch(function (error) {
+    console.log("Error: " + error);
+});
+         
+        
+    
+
+
